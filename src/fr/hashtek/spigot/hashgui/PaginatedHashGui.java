8317abf7ac10899b31b20b506b4f552c4332b071
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-04-16 23:58:21 +0200

    feat(hashgui): Added Paginated GUIs. Fucking happy :>

--- src/fr/hashtek/spigot/hashgui/PaginatedHashGui.java
+++ src/fr/hashtek/spigot/hashgui/PaginatedHashGui.java
@@ -1,10 +1,10 @@
 package fr.hashtek.spigot.hashgui;
 
-import fr.hashtek.spigot.hashgui.handler.click.ClickAction;
 import fr.hashtek.spigot.hashgui.handler.click.ClickHandler;
 import fr.hashtek.spigot.hashgui.manager.HashGuiManager;
 import fr.hashtek.spigot.hashgui.page.HashPage;
 import fr.hashtek.spigot.hashitem.HashItem;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -15,13 +15,13 @@ import java.util.List;
 public class PaginatedHashGui extends HashGui
 {
 
-    private final List<HashPage> pages;
-
+    private List<HashPage> pages;
+    private List<Integer> freeSlotIndexes;
     private int currentPageIndex;
 
 
     /**
-     * Creates a new GUI.
+     * Creates a new Paginated GUI.
      *
      * @param	title	GUI's title
      * @param	size	GUI's amount of lines (must be between 1 and 6).
@@ -29,31 +29,62 @@ public class PaginatedHashGui extends HashGui
     public PaginatedHashGui(String title, int size)
     {
         super(title, size);
-
-        this.pages = new ArrayList<HashPage>();
+        this.setup();
     }
 
     /**
-     * Creates a new instance of HashGUI from an existing Inventory.
+     * Creates a new Paginated GUI from an existing Inventory.
      *
      * @param	inventory	Inventory
      */
     public PaginatedHashGui(Inventory inventory)
     {
         super(inventory);
-
-        this.pages = new ArrayList<HashPage>();
+        this.setup();
     }
 
 
-    public void setPreviousPageItem(HashItem item, HashGuiManager guiManager)
+    /**
+     * Setups the Paginated GUI.
+     */
+    private void setup()
+    {
+        this.pages = new ArrayList<HashPage>();
+        this.createNewPage();
+        this.setCurrentPageIndex(0);
+    }
+
+    /**
+     * Finds and puts in a list every free slot indexes.
+     */
+    private void findFreeSlotIndexes()
+    {
+        this.freeSlotIndexes = new ArrayList<Integer>();
+
+        for (int k = 0; k < super.getTotalSize(); k++) {
+            final ItemStack item = super.getInventory().getItem(k);
+            if (item == null || item.getType() == Material.AIR)
+                this.freeSlotIndexes.add(k);
+        }
+    }
+
+    /**
+     * Sets the item as the previous page button.
+     * FIXME: In the click action, if gui is not an instance of
+     *        PaginatedHashGui, we may want to throw an error.
+     *
+     * @param   item        Item
+     * @param   guiManager  Gui manager
+     * @return  Returns itself.
+     */
+    public PaginatedHashGui setPreviousPageItem(HashItem item, HashGuiManager guiManager)
     {
         item.addClickHandler(
             new ClickHandler()
                 .addAllClickTypes()
                 .setClickAction((Player player, HashGui gui, ItemStack i, int slot) -> {
                     if (!(gui instanceof PaginatedHashGui))
-                        return; // FIXME: Maybe an error.
+                        return;
 
                     final PaginatedHashGui paginatedGui = (PaginatedHashGui) gui;
                     final int pageIndex = paginatedGui.getCurrentPageIndex() - 1;
@@ -62,60 +93,155 @@ public class PaginatedHashGui extends HashGui
                         return;
 
                     paginatedGui.setCurrentPageIndex(pageIndex);
-                    paginatedGui.updatePage(player);
+                    paginatedGui.update(player);
                 })
         );
 
         item.build(guiManager);
+        return this;
     }
 
-    public void setNextPageItem(HashItem item, HashGuiManager guiManager)
+    /**
+     * Sets the item as the next page button.
+     * FIXME: In the click action, if gui is not an instance of
+     *        PaginatedHashGui, we may want to throw an error.
+     *
+     * @param   item        Item
+     * @param   guiManager  Gui manager
+     * @return  Returns itself.
+     */
+    public PaginatedHashGui setNextPageItem(HashItem item, HashGuiManager guiManager)
     {
         item.addClickHandler(
             new ClickHandler()
                 .addAllClickTypes()
                 .setClickAction((Player player, HashGui gui, ItemStack i, int slot) -> {
                     if (!(gui instanceof PaginatedHashGui))
-                        return; // FIXME: Maybe an error.
+                        return;
 
                     final PaginatedHashGui paginatedGui = (PaginatedHashGui) gui;
                     final int pageIndex = paginatedGui.getCurrentPageIndex() + 1;
 
-                    if (pageIndex > this.pages.size())
+                    if (pageIndex >= this.pages.size())
                         return;
 
                     paginatedGui.setCurrentPageIndex(pageIndex);
-
-                    paginatedGui.updatePage(player);
+                    paginatedGui.update(player);
                 })
         );
 
         item.build(guiManager);
+        return this;
     }
 
-    public void updatePage(Player player)
+    /**
+     * Updates the GUI according to the current page.
+     *
+     * @param   player  Player
+     * @return  Returns itself.
+     */
+    @Override
+    public PaginatedHashGui update(Player player)
     {
         final HashPage currentPage = this.getCurrentPage();
 
+        this.clearPageContent();
         for (int slot : currentPage.getItems().keySet()) {
             final HashItem item = currentPage.getItem(slot);
-
-            this.setItem(slot, item);
+            super.setItem(slot, item);
         }
 
-        this.update(player);
+        super.update(player);
+        return this;
     }
 
+    /**
+     * Visually clears every free slot.
+     *
+     * @return  Returns itself.
+     */
+    public PaginatedHashGui clearPageContent()
+    {
+        if (this.freeSlotIndexes == null)
+            this.findFreeSlotIndexes();
+
+        for (int k : this.freeSlotIndexes)
+            super.getInventory().setItem(k, null);
+        return this;
+    }
+
+    /* Page management */
+
+    /**
+     * Creates a fresh new page.
+     *
+     * @return  Created page
+     */
+    public HashPage createNewPage()
+    {
+        final HashPage page = new HashPage(this);
+
+        this.pages.add(page);
+        return page;
+    }
+
+    /**
+     * Clears all pages.
+     *
+     * @return  Returns itself.
+     */
+    public PaginatedHashGui clearPages()
+    {
+        this.pages.clear();
+        this.createNewPage();
+        this.setCurrentPageIndex(0);
+        return this;
+    }
+
+    /**
+     * @param   index  Page index
+     * @return  Page at index
+     */
+    public HashPage getPage(int index)
+    {
+        return this.pages.get(index);
+    }
+
+    /**
+     * @return  All pages
+     */
+    public List<HashPage> getPages()
+    {
+        return this.pages;
+    }
+
+    /**
+     * @return  Last page
+     */
+    public HashPage getLastPage()
+    {
+        return this.pages.get(this.pages.isEmpty() ? 0 : this.pages.size() - 1);
+    }
+
+    /**
+     * @return  Current page
+     */
     public HashPage getCurrentPage()
     {
-        return this.pages.get(this.currentPageIndex);
+        return this.getPage(this.getCurrentPageIndex());
     }
 
+    /**
+     * @return  Current page index
+     */
     public int getCurrentPageIndex()
     {
         return this.currentPageIndex;
     }
 
+    /**
+     * @param   index   Page index
+     */
     public void setCurrentPageIndex(int index)
     {
         this.currentPageIndex = index;
--- src/fr/hashtek/spigot/hashgui/page/HashPage.java
+++ src/fr/hashtek/spigot/hashgui/page/HashPage.java
@@ -25,41 +25,85 @@ public class HashPage
     }
 
 
-    private boolean checkItem(int slot)
+    /**
+     * @param   slot    Slot index
+     * @return  Returns true if an item is already at this slot.
+     */
+    private boolean isItemInParentGui(int slot)
     {
-        final ItemStack i = this.parent.getInventory().getItem(slot);
+        final ItemStack item = this.parent.getInventory().getItem(slot);
 
-        return i == null || i.getType() == Material.AIR;
+        return item != null && item.getType() != Material.AIR;
     }
 
-    public void setItem(int slot, HashItem item)
-        throws IllegalArgumentException
+    /**
+     * Sets an item at a given slot.
+     *
+     * @param   slot                        Slot index
+     * @param   item                        Item
+     * @throws  IllegalArgumentException    Slot already in use
+     */
+    public void setItem(int slot, HashItem item) throws IllegalArgumentException
     {
-        if (!checkItem(slot))
+        if (isItemInParentGui(slot))
             throw new IllegalArgumentException("Item can't be placed at the slot " + slot + ".");
 
         this.items.put(slot, item);
     }
 
-    public void removeItem(int slot)
-        throws IllegalArgumentException
+    /**
+     * Adds an item to the page.
+     *
+     * @param   item                        Item
+     * @throws  IllegalArgumentException    No slot left
+     */
+    public void addItem(HashItem item) throws IllegalArgumentException
     {
-        if (!checkItem(slot))
+        for (int k = 0; k < this.parent.getTotalSize(); k++) {
+            if (isItemInParentGui(k) || this.items.containsKey(k))
+                continue;
+
+            this.items.put(k, item);
+            return;
+        }
+
+        throw new IllegalArgumentException("No slot left.");
+    }
+
+    /**
+     * Removes an item from the page.
+     *
+     * @param   slot                        Slot index
+     * @throws  IllegalArgumentException    Unreachable slot (not in the page but in the parent GUI)
+     */
+    public void removeItem(int slot) throws IllegalArgumentException
+    {
+        if (isItemInParentGui(slot))
             throw new IllegalArgumentException("Item can't be removed from the slot " + slot + ".");
 
         this.items.remove(slot);
     }
 
+    /**
+     * Clears page items.
+     */
     public void clearItems()
     {
         this.items.clear();
     }
 
+    /**
+     * @param   slot    Slot index
+     * @return  Item at slot
+     */
     public HashItem getItem(int slot)
     {
         return this.items.get(slot);
     }
 
+    /**
+     * @return  Page items
+     */
     public Map<Integer, HashItem> getItems()
     {
         return this.items;
